2d7a8916fb951d7a0bde0e406fd060d97350c872
axelquartz <axelarep@gmail.com>
2024-10-30 00:18:20 -0600

    Init main files

---
+++ script.js
@@ -0,0 +1,9 @@
+// Create an array for each list
+
+// Create a task template (object or class): name, description, method to move task to other list
+
+// Manually create a new task
+
+// Move a task from one list to another
+
+// Create task from user prompt
